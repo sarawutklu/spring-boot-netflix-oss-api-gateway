df4b68588b0bdd6907434e2e39030d95c012724e
DESKTOP-VUB0V1K\KO <sarawut@jpwall.com>
2021-01-21 23:11:11 +0700

    nitiali code

---
+++ zuul-api-gateway/src/main/java/github/sarawutklu/zuulapigateway/ZuulApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package github.sarawutklu.zuulapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuulApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulApiGatewayApplication.class, args);
+	}
+
+}
